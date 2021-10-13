9ee25a5f15f27eb4a1b183ad528cf711df91a058
zhoufeng <zhf883680@gmail.com>
2021-10-13 16:45:50 +0800

    增加图标 针对谷歌,百度做特别处理

--- background.js
+++ background.js
@@ -15,6 +15,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
 });
 const colors = ["grey", "blue", "yellow", "red", "green", "pink", "purple", "cyan", "green", "blue", "grey"]
+const specialUrl = ["google.com","baidu.com"]//特殊处理的地址
 async function createGroup(tab) {
     //获取当前的所有tab
     //let groups =[];
@@ -24,31 +25,42 @@ async function createGroup(tab) {
         try {
               //获取tab对应的域名
             const host = tab.url.split("/")[2];
-            const domainArr = host.split(".")
             let domain = "";
-            if (domainArr.length == 1) {
-                domain = domainArr[0];
+            //针对设置页面特殊处理
+            if(host=="edge"||host=="chrome"){
+                domain="设置";
             }
-            if (domainArr.length >= 2) {
-                domain = `${domainArr[domainArr.length-2]}.${domainArr[domainArr.length-1]}`;
+            else if(host=="http"||host=="https"){
+                //正常页面
+                const domainArr = host.split(".")
+                if (domainArr.length == 1) {
+                    domain = domainArr[0];
+                }
+                if (domainArr.length >= 2) {
+                   
+                    domain = `${domainArr[domainArr.length-2]}.${domainArr[domainArr.length-1]}`;
+                    //特殊处理
+                    //例如谷歌邮件 谷歌搜索之类
+                    if(specialUrl.find(a=>a==domain)){
+                        domain = `${domainArr[domainArr.length-3]}.${domainArr[domainArr.length-2]}.${domainArr[domainArr.length-1]}`;
+                    }
+                }
+            }
+            else{
+                domain="特殊";
             }
             //检查是否有旧group
             const nowGroup = groups.find(a => a.title == domain);
             const groupId = parseInt(new Date().getTime().toString().substring(4));
-    
+            //无旧组件
             if (nowGroup == undefined) {
                 chrome.tabs.group({
                     createProperties: {
                         windowId: tab.windowId,
-                        //id:groupId,
-                        //color:colors[parseInt(Math.random()*10)]
                     },
-                    // groupId: groupId,
                     tabIds: tab.id
                 }, function (groupId) {
-                    //将此group
-                    console.log("创建了Id" + groupId)
-                    console.log(domain)
+                    //将此group设置标题和颜色
                     chrome.tabGroups.update(groupId, {
                         color: colors[parseInt(Math.random() * 10)],
                         title: domain,
@@ -60,18 +72,6 @@ async function createGroup(tab) {
                     tabIds: tab.id
                 })
             }
-            //将当前的tab移入
-            //createProperties
-            //createProperties
-    
-            // chrome.tabs.group({
-            //     groupId: nowGroup == undefined?groupId:nowGroup.id,
-            //     tabIds: tab.id
-            // },function(returnGroupId){
-            //     console.log(returnGroupId)
-            // });
-            //chrome.tabs.group();
-    
         } catch (e) {
             console.error(e)
         }
