e588ab415e4c7cb915ed44aeec0e94a862ee2230
zhoufeng <zhf883680@gmail.com>
2022-07-08 09:38:55 +0800

    修复未自动分组的问题

--- background.js
+++ background.js
@@ -37,95 +37,101 @@ function createGroup(tab) {
                         }
                         else{
                             console.debug("是否分组结束")
-                            try {
-                                const urlHead = tab.url.split("/")[0];
-                                console.debug(urlHead)
-                                //获取tab对应的域名
-                                const host = tab.url.split("/")[2].split(":")[0];
-                                let domain = "";
-                                //针对设置页面特殊处理
-                                if (urlHead == "edge:" || urlHead == "chrome:" || urlHead.indexOf("extension:") > -1 || urlHead.indexOf("file:") > -1) {
-                                    domain = "~ " + urlHead.substring(0, urlHead.length - 1);
-                                } else if (urlHead == "http:" || urlHead == "https:") {
-                                    //正常页面
-                                    const domainArr = host.split(".")
-                                    if (domainArr.length == 1) {
-                                        domain = domainArr[0];
-                                    }
-                                    if (domainArr.length >= 2) {
-                
-                                        domain = `${domainArr[domainArr.length - 2]}.${domainArr[domainArr.length - 1]}`;
-                                        //特殊处理
-                                        //例如谷歌搜索,百度搜索为一类 其他为另一类
-                                        if (tab.url.indexOf("www.google.com") > -1 || tab.url.indexOf("www.baidu.com") > -1) {
-                                            //domain = `${domainArr[domainArr.length-3]}.${domainArr[domainArr.length-2]}.${domainArr[domainArr.length-1]}`;
-                                            domain = "~ search"
-                                        }
-                                        //org.cn  com.cn之类统一处理
-                                        if (domainArr[domainArr.length - 1] == "cn" && domainArr.length > 3) {
-                                            domain = `${domainArr[domainArr.length - 3]}.${domainArr[domainArr.length - 2]}`;
-                                        }
-                                    }
-                                } else {
-                                    domain = "~ UnKnow"
-                                }
-                                domain = domain + " (auto)"
-                                //检查是否有旧group
-                                const nowGroup = groups.find(a => a.title == domain);
-                                console.debug("开始设置组")
-                                //无旧组
-                                if (nowGroup == undefined) {
-                                    chrome.tabs.group({
-                                        createProperties: {
-                                            windowId: currentWindow.id,
-                                        },
-                                        tabIds: tab.id
-                                    }, function (groupId) {
-                                        console.debug("开始调整创建组")
-                                        //将此group设置标题和颜色
-                                        chrome.tabGroups.update(groupId, {
-                                            color: colors[parseInt(Math.random() * 10)],
-                                            title: domain,
-                                        });
-                                        //将组别排序
-                                        //系统级别最先
-                                        //其次是搜索
-                                        //其次从a-z排序
-                                        console.debug("开始排序组")
-                                        chrome.tabGroups.query({
-                                            windowId: currentWindow.id
-                                        }).then((groups) => {
-                                            let nowGroup = JSON.parse(JSON.stringify(groups));
-                                            nowGroup.sort(sortBy("title", false, String))
-                                            console.debug("开始移动组")
-                                            for (let i = 0; i < nowGroup.length; i++) {
-                                                console.debug(nowGroup[i]);
-                                                chrome.tabGroups.move(nowGroup[i].id, {
-                                                    index: -1
-                                                });
-                                            }
-                                        })
-                                    })
-                                } else {
-                                    //有就组
-                                    console.debug("直接更新")
-                                    chrome.tabs.group({
-                                        groupId: nowGroup.id,
-                                        tabIds: tab.id
-                                    })
-                                }
-                
-                
-                            } catch (e) {
-                                console.error(e)
-                            }
+                            createGroupReal(tab,groups,currentWindow);
                         }
                     }
                 )
             }
+            else{
+                createGroupReal(tab,groups,currentWindow);
+            }
           
         })
     });
+}
+function createGroupReal(tab,groups,currentWindow){
+    try {
+        const urlHead = tab.url.split("/")[0];
+        console.debug(urlHead)
+        //获取tab对应的域名
+        const host = tab.url.split("/")[2].split(":")[0];
+        let domain = "";
+        //针对设置页面特殊处理
+        if (urlHead == "edge:" || urlHead == "chrome:" || urlHead.indexOf("extension:") > -1 || urlHead.indexOf("file:") > -1) {
+            domain = "~ " + urlHead.substring(0, urlHead.length - 1);
+        } else if (urlHead == "http:" || urlHead == "https:") {
+            //正常页面
+            const domainArr = host.split(".")
+            if (domainArr.length == 1) {
+                domain = domainArr[0];
+            }
+            if (domainArr.length >= 2) {
+
+                domain = `${domainArr[domainArr.length - 2]}.${domainArr[domainArr.length - 1]}`;
+                //特殊处理
+                //例如谷歌搜索,百度搜索为一类 其他为另一类
+                if (tab.url.indexOf("www.google.com") > -1 || tab.url.indexOf("www.baidu.com") > -1) {
+                    //domain = `${domainArr[domainArr.length-3]}.${domainArr[domainArr.length-2]}.${domainArr[domainArr.length-1]}`;
+                    domain = "~ search"
+                }
+                //org.cn  com.cn之类统一处理
+                if (domainArr[domainArr.length - 1] == "cn" && domainArr.length > 3) {
+                    domain = `${domainArr[domainArr.length - 3]}.${domainArr[domainArr.length - 2]}`;
+                }
+            }
+        } else {
+            domain = "~ UnKnow"
+        }
+        domain = domain + " (auto)"
+        //检查是否有旧group
+        const nowGroup = groups.find(a => a.title == domain);
+        console.debug("开始设置组")
+        //无旧组
+        if (nowGroup == undefined) {
+            chrome.tabs.group({
+                createProperties: {
+                    windowId: currentWindow.id,
+                },
+                tabIds: tab.id
+            }, function (groupId) {
+                console.debug("开始调整创建组")
+                //将此group设置标题和颜色
+                chrome.tabGroups.update(groupId, {
+                    color: colors[parseInt(Math.random() * 10)],
+                    title: domain,
+                });
+                //将组别排序
+                //系统级别最先
+                //其次是搜索
+                //其次从a-z排序
+                console.debug("开始排序组")
+                chrome.tabGroups.query({
+                    windowId: currentWindow.id
+                }).then((groups) => {
+                    let nowGroup = JSON.parse(JSON.stringify(groups));
+                    nowGroup.sort(sortBy("title", false, String))
+                    console.debug("开始移动组")
+                    for (let i = 0; i < nowGroup.length; i++) {
+                        console.debug(nowGroup[i]);
+                        chrome.tabGroups.move(nowGroup[i].id, {
+                            index: -1
+                        });
+                    }
+                })
+            })
+        } else {
+            //有就组
+            console.debug("直接更新")
+            chrome.tabs.group({
+                groupId: nowGroup.id,
+                tabIds: tab.id
+            })
+        }
+
+
+    } catch (e) {
+        console.error(e)
+    }
 }
 //排序函数
 var sortBy = function (filed, rev, primer) {
